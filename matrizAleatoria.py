38d1030285b7315e7745222f6850af46baabb514
carlosag16 <carloswiiu@gmail.com>
2022-05-03 18:41:19 -0300

    correções

--- matrizAleatoria.py
+++ matrizAleatoria.py
@@ -1,28 +1,27 @@
+from ast import Break
 import numpy as np
 import random
-nlin = int(input('insira o numero de colunas \n'))
-ncol = int(input('insira o numero de linhas \n'))
+nlin = int(input('insira o numero de linhas \n'))
+ncol = int(input('insira o numero de colunas \n'))
 if(nlin and ncol > 0):
-  a = np.arange(nlin*ncol).reshape(nlin, ncol)
-  for w in range(ncol):
-      for h in range(nlin):
-          a[w, h] = random.randint(0, 10)
+  #a = np.arange(nlin*ncol).reshape((nlin, ncol),order='F')
+  a = np.random.randint(10, size=(nlin, ncol)) 
   print(f'a matriz gerada aleatoriameente é: \n {a} \n')
   b_lin = []
   b_col = []
-  for i in range(0, nlin):
-      for j in range(0, ncol):
+  for i in range( nlin):
+      for j in range( ncol):
           if (a[i, j] == 0):
 
               b_lin.append(i)
               b_col.append(j)
 
   for i in b_lin:
-      for k1 in range(0, ncol):
+      for k1 in range( ncol):
           a[i, k1] = 0
 
   for i in b_col:
-      for k1 in range(0, nlin):
+      for k1 in range( nlin):
           a[k1, i] = 0
   print(f'a nova matriz após a modificação é: \n{a}\n')
 else:
